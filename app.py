633785516b168d8e16a95bbd74c4e20e86ac1662
kumardeepak07 <39013184+kumardeepak07@users.noreply.github.com>
2022-01-12 20:06:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, request, jsonify, render_template
+import util
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('WebPage.html')
+
+
+@app.route('/Cassava_Leaf_Disease_Detection', methods=['GET', 'POST'])
+def classify_image():
+    image_data = request.form['image_data']
+
+    response = jsonify(util.classify_image(image_data))
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    util.load_saved_artifacts()
+    app.run(debug=True)
+
